25c6e9933965a29e18800c2f0615ec4d70cdfdd2
Patriziano <onahpatrick75@gmail.com>
2023-10-13 16:10:21 -0700

    different classes inherited from the BaseModel class

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The City module"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    The City class inherited from BaseModel
+    """
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The state module"""
+from models.base_model import BaseModel
+
+
+class State:
+    """
+    The State class that is inherited from BaseModel
+    """
+    name = ""
