f19a8239cd9637f3619b93e19b327adca7f55913
Patriziano <onahpatrick75@gmail.com>
2023-10-16 13:35:08 -0700

    More test cases for FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,10 +2,13 @@
 """Tests for file_storage.py"""
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models import storage
 
 
 class Test_FileStorage(unittest.TestCase):
     """Tests for the FileStorage class"""
+    model = BaseModel()
 
     def test_is_subclass(self):
         """Tests if FileStorage is a subclass of object"""
@@ -15,3 +18,19 @@ class Test_FileStorage(unittest.TestCase):
         """Tests if an object is an instance of FileStorage"""
         file_1 = FileStorage()
         self.assertIsInstance(file_1, FileStorage)
+
+    def New_reload(self):
+        """ testing the reload """
+        storage._FileStorage__file_path = "file.json"
+        storage.reload()
+
+    def test_Has_Attributes(self):
+        """Testing the filestorage attributes """
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
+
+    def test_new(self):
+        """tests if the method is able to save an obj to
+        __objects dictionary with the format <classname>.id"""
+        self.assertIn("{}.{}".format("BaseModel", Test_FileStorage.model.id),
+                      storage._FileStorage__objects)
